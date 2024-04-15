6ad98767838ed901da322309b39b27c522f1ad86
Yurii Shkrobut <georgebest2409@gmail.com>
2024-04-15 17:44:50 +0300

    make build

---
+++ index.d35d78a1.js
@@ -0,0 +1,2 @@
+var e=document.querySelector(".wall"),t=document.querySelector(".spider");e.style.display="flex",e.style.justifyContent="center",e.style.alignItems="center",t.style.position="relative";
+//# sourceMappingURL=index.d35d78a1.js.map
---
+++ index.f75de5e1.js
@@ -0,0 +1,10 @@
+/* eslint-disable no-useless-computed-key */ "use strict";
+// write code here
+const wall = document.querySelector(".wall");
+const spider = document.querySelector(".spider");
+wall.style.display = "flex";
+wall.style.justifyContent = "center";
+wall.style.alignItems = "center";
+spider.style.position = "relative";
+
+//# sourceMappingURL=index.f75de5e1.js.map
